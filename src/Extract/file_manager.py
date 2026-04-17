317731e03ac87564aba2c4424dd6ea60cf3ee823
3BoOoD97 <abodn70@hotmail.com>
2026-04-18 00:54:59 +0700

    Add extra comments to improve code readability

--- src/Extract/file_manager.py
+++ src/Extract/file_manager.py
@@ -13,11 +13,11 @@ class FileManager:
         self.output_gz_data_path = os.path.join("./output", f"{self.dataset_name}.tsv.gz")
         self.last_download_file_date = f"./output/last_download_date/{self.dataset_name}.txt"
 
-    # This function check if the data file exists locally or not
+    # This function checks if the data file exists locally or not
     def data_file_exists(self):
         return os.path.exists(self.output_gz_data_path)
 
-
+    # This function saves the stream coming from Eurostat locally as a .gz file.
     def save_file(self, stream):
         try:
             # Prepare the file to write the data in binary access mode
@@ -29,15 +29,14 @@ class FileManager:
         # Catch general exceptions
         except Exception as e:
             raise IOError(f"Error saving file: {e}")
-
+        # Make sure the data file exists locally
         if not self.data_file_exists():
             raise FileNotFoundError("Data file was not created")
-
+        # Make sure the data file is not empty
         if self.get_data_file_size_mb() == 0:
             raise ValueError("Downloaded data file is empty")
 
-
-
+    # This function returns the size of the local data file in MB if it exists
     def get_data_file_size_mb(self):
         if not self.data_file_exists():
             return 0
