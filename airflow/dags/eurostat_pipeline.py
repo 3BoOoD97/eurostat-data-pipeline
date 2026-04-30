06476f05de6e1802430bd5c2a1d8288c19e85623
3BoOoD97 <abodn70@hotmail.com>
2026-04-30 22:41:40 +0700

    Add basic Airflow tasks to test extract and transform

---
+++ airflow/dags/eurostat_pipeline.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="Eurostat_pipeline",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    task_1 = BashOperator(
+        task_id="Extract_pipline",
+        bash_command="docker run --rm -v /run/desktop/mnt/host/c/Users/Hp/Desktop/THESIS/CODE/output:/app/output code-extract migr_asyapp1mp"
+    )
+
+    task_2 = BashOperator(
+        task_id="Transform_pipline",
+        bash_command="docker run --rm -v /run/desktop/mnt/host/c/Users/Hp/Desktop/THESIS/CODE/output:/app/output code-transform migr_asyapp1mp"
+    )
+
+
+
+    task_1 >> task_2
